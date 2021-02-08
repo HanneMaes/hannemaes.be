9887f4d6ab09a4e1a6bd5a99e0505e7e66963eea
Hanne Maes <maes.hanne@gmail.com>
2021-02-08 14:35:38 +0100

    small tweaks

--- docs/script.js
+++ docs/script.js
@@ -1,33 +1,35 @@
 /* remove cables.gl iframe */
-console.log('remove bg before');
-document.getElementById("cables_gl").contentWindow.document.querySelector('footer').style.display = "none";
-console.log('remove bg after');
+// console.log('remove bg before');
+// document.getElementById("cables_gl").contentWindow.document.querySelector('footer').style.display = "none";
+// console.log('remove bg after');
 
+/* ***************** */
 /* 3D card transform */
+/* ***************** */
 
-let constrain = 20;
-let mouseOverContainer = document.getElementById("ex1");
-let ex1Layer = document.getElementById("ex1-layer");
+// let constrain = 20;
+// let mouseOverContainer = document.getElementById("ex1"); // document.getElementsByTagName('body')[0]; 
+// let ex1Layer = document.getElementById("ex1-layer");
 
-function transforms(x, y, el) {
-  let box = el.getBoundingClientRect();
-  let calcX = -(y - box.y - (box.height / 2)) / constrain;
-  let calcY = (x - box.x - (box.width / 2)) / constrain;
+// mouseOverContainer.onmousemove = function(e) {
+//   let xy = [e.clientX, e.clientY];
+//   let position = xy.concat([ex1Layer]);
+
+//   window.requestAnimationFrame(function(){
+//     transformElement(ex1Layer, position);
+//   });
+// };
+
+// function transforms(x, y, el) {
+//   let box = el.getBoundingClientRect();
+//   let calcX = -(y - box.y - (box.height / 2)) / constrain;
+//   let calcY = (x - box.x - (box.width / 2)) / constrain;
   
-  return "perspective(1000px) "
-    + "   rotateX("+ calcX +"deg) "
-    + "   rotateY("+ calcY +"deg) ";
-};
+//   return "perspective(1000px) "
+//     + "   rotateX("+ calcX +"deg) "
+//     + "   rotateY("+ calcY +"deg) ";
+// };
 
- function transformElement(el, xyEl) {
-  el.style.transform  = transforms.apply(null, xyEl);
-}
-
-mouseOverContainer.onmousemove = function(e) {
-  let xy = [e.clientX, e.clientY];
-  let position = xy.concat([ex1Layer]);
-
-  window.requestAnimationFrame(function(){
-    transformElement(ex1Layer, position);
-  });
-};
+//  function transformElement(el, xyEl) {
+//   el.style.transform  = transforms.apply(null, xyEl);
+// }
