f78a4377fe4a6d6d3c7daf7b0bada30ec1ebdfb0
Hanne Maes <maes.hanne@gmail.com>
2021-02-09 21:20:25 +0100

    small tweaks

--- docs/script.js
+++ docs/script.js
@@ -7,29 +7,29 @@
 /* 3D card transform */
 /* ***************** */
 
-// let constrain = 20;
-// let mouseOverContainer = document.getElementById("ex1"); // document.getElementsByTagName('body')[0]; 
-// let ex1Layer = document.getElementById("ex1-layer");
+let constrain = 20;
+let mouseOverContainer = document.getElementById("ex1"); // document.getElementsByTagName('body')[0]; 
+let ex1Layer = document.getElementById("ex1-layer");
 
-// mouseOverContainer.onmousemove = function(e) {
-//   let xy = [e.clientX, e.clientY];
-//   let position = xy.concat([ex1Layer]);
+mouseOverContainer.onmousemove = function(e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([ex1Layer]);
 
-//   window.requestAnimationFrame(function(){
-//     transformElement(ex1Layer, position);
-//   });
-// };
+  window.requestAnimationFrame(function(){
+    transformElement(ex1Layer, position);
+  });
+};
 
-// function transforms(x, y, el) {
-//   let box = el.getBoundingClientRect();
-//   let calcX = -(y - box.y - (box.height / 2)) / constrain;
-//   let calcY = (x - box.x - (box.width / 2)) / constrain;
+function transforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(y - box.y - (box.height / 2)) / constrain;
+  let calcY = (x - box.x - (box.width / 2)) / constrain;
   
-//   return "perspective(1000px) "
-//     + "   rotateX("+ calcX +"deg) "
-//     + "   rotateY("+ calcY +"deg) ";
-// };
+  return "perspective(1000px) "
+    + "   rotateX("+ calcX +"deg) "
+    + "   rotateY("+ calcY +"deg) ";
+};
 
-//  function transformElement(el, xyEl) {
-//   el.style.transform  = transforms.apply(null, xyEl);
-// }
+ function transformElement(el, xyEl) {
+  el.style.transform  = transforms.apply(null, xyEl);
+}
